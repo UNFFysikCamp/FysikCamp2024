bafd732fdba853c12ff6ed2df01541d053bf32af
Mikkel <MikkelMM99@gmail.com>
2024-05-17 17:41:40 +0200

    Intro, opgaver og advanceret data

--- bash_script/save_to_drive.sh
+++ bash_script/save_to_drive.sh
@@ -4,17 +4,19 @@ cd /content/$1
 # Change the year here
 source_folder="/content/"$1
 
+
+echo "Copying main folder to Google Drive"
 # Folder to exclude from copying
-
-
 excluded_files=(".gitignore" "README.md" "HER.ipynb")
-
 #Change the year here
 destination_folder="/content/drive/MyDrive/Colab Notebooks/"$1
 # Copy folders and contents excluding the specified folder and files, without overwriting existing files
 rsync -av --exclude="bash_script" --exclude=".git" --exclude-from=<(printf "%s\n" "${excluded_files[@]}") --ignore-existing "$source_folder/" "$destination_folder/"
 
 echo "Finished creating main folder"
+
+
+echo "Copying source folder to Google Drive"
 excluded_files=(".gitignore" "README.md")
 #Change the year here
 destination_folder="/content/drive/MyDrive/Colab Notebooks/"$1"/source"
